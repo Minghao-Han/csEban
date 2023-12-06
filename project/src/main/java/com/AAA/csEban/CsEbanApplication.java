80ead1e87856638c33a059200ef183da62b863ee
Minghao-Han <3508627758@qq.com>
2023-12-06 11:13:26 +0800

    initialize spring boot project

---
+++ project/src/main/java/com/AAA/csEban/CsEbanApplication.java
@@ -0,0 +1,13 @@
+package com.AAA.csEban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsEbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsEbanApplication.class, args);
+	}
+
+}
---
+++ project/src/test/java/com/AAA/csEban/CsEbanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AAA.csEban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsEbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
